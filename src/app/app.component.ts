fbf1310c70a39aaa3e0143403a4ddf3a4c79f366
Freddy Achu Gaspar <freddycador321@gmail.com>
2025-05-23 17:30:59 -0400

    Conexion al servidor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Usuario } from './models/usuario';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor( private http:HttpClient) { }
+  usuarios:Usuario [] = []
+  ngOnInit(): void {
+    console.log("Ingrese hasta aqui")
+    this.ver().subscribe((data:any)=>{
+      this.usuarios = data
+      console.log (data)
+    })
+  }
   title = 'vista';
+  ver() {
+    return this.http.get('http://127.0.0.1:8000/api/usuario');
+  }
 }
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,9 @@
+export interface Usuario {
+        nombre:string,
+        apellido:string,
+        username:string,
+        password:string,
+        email:string,
+        acceso:string
+        id:number
+}
